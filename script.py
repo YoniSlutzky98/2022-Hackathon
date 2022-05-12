1fc8727947cbbbbb11acb5b2af95a86852583f60
Nitz-alt <nitzanyizhar@gmail.com>
2022-05-12 18:33:54 +0300

    Google drive scripts added

---
+++ script.py
@@ -0,0 +1,48 @@
+from fileinput import filename
+from turtle import down
+import driveaccess
+import io
+from googleapiclient.http import MediaIoBaseDownload
+
+def downloadFile(drive_service, file):
+    request = drive_service.files().get_media(fileId=file['id'])
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with open("{}".format(file['name']), "wb") as f:
+        f.write(fh.getbuffer())
+
+def search_files(drive_service, key_words, course_id):
+    service = drive_service
+    page_token = None
+    total_files = []
+    while True:
+        query = "mimeType='application/pdf' and name contains '{0}'".format(key_words)
+        response = service.files().list(q=query,
+                                          spaces='drive',
+                                          fields='nextPageToken, files(id, name)',
+                                          pageToken=page_token).execute()
+        for file in response.get('files', []):
+            # Process change
+            print('Found file: %s (%s)' % (file.get('name'), file.get('id')))
+            total_files.append(file)
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+
+
+
+
+
+if __name__ == '__main__':
+    file_name = ""
+    service = quickstart.access_drive()
+    res = service.files().list(q=("mimeType='application/pdf'") , spaces="drive", fields="nextPageToken, files(id, name, webViewLink)").execute()
+    for file in res.get('files', []):
+        print(file)
+    
+
+        
